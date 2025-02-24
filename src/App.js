6899253e3754a0c4576604f81c80cdec4bb63887
Devendar-123 <devendarnenavath28@gmail.com>
2025-02-24 12:19:51 +0530

    Modified by installing npm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 // import First from "./components/first/first";
 import "./App.css"
-// import Practice from "./components/useEffect/Practice";
+import Practice from "./components/useEffect/Practice";
 // import Parent from "./components/CounterApp/Parent";
-import Practice from "./components/practice/Practice";
+// import Practice from "./components/practice/Practice";
 function App() {
   return (
     <div className="appDiv">
       {/* This is the div in app.js */}
       {/* <First/> */}
       {/* <Parent/> */}
-      {/* <Practice/> */}
       <Practice/>
+      {/* <Practice/> */}
     </div>
   );
 }
